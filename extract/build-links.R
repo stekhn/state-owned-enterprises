1ad88908ae5a65174e76bfdd456787292a72d30a
Oliver Schnuck <oliver.schnuck@br.de>
2016-10-27 16:53:42 +0200

    add filter to build links

--- extract/build-links.R
+++ extract/build-links.R
@@ -4,5 +4,13 @@ direct <- read.csv(file="./direct_ownership.csv",head=TRUE,sep=",")
 indirect <- read.csv(file="indirect_ownership.csv",head=TRUE,sep=",")
 
 master <- right_join(indirect, direct, by=c("name", "parent"))
-                     
+
+ids <- master %>% select(id)
+
+for (i in ids) {
+  master %>%
+  filter(id=i) %>%
+    summarize( count = n() )
+}
+
 
